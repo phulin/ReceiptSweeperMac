cce1c9f06b16395ec76a543166e30c6ea1da7eca
Patrick Hulin <patrick.hulin@gmail.com>
2026-02-20 11:24:38 -0500

    Initial Commit

---
+++ ReceiptSweeperMac/ReceiptSweeperMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptSweeperMacApp.swift
+//  ReceiptSweeperMac
+//
+//  Created by Patrick Hulin on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptSweeperMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
